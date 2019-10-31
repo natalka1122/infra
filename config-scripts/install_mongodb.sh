64edd97cacae1c7f14c7c55c048db1370e3d4dfa
natalka1122 <natalka1122@yandex.ru>
2019-10-31 22:47:52 +0300

    refactor

--- config-scripts/install_mongodb.sh
+++ config-scripts/install_mongodb.sh
@@ -1,6 +1,6 @@
-wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
-echo "deb [ arch=amd64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
-sudo apt-get update
-sudo apt-get install -y mongodb-org
-sudo systemctl start mongod
-sudo systemctl enable mongod
+wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | apt-key add -
+echo "deb [ arch=amd64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.2 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-4.2.list
+apt-get update
+apt-get install -y mongodb-org
+systemctl start mongod
+systemctl enable mongod
