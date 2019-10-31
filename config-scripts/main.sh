3b2dcb574ea08c719761cbe058488a2ed0e0d750
natalka1122 <natalka1122@yandex.ru>
2019-10-31 15:26:06 +0300

    terraform in progress

--- config-scripts/main.sh
+++ config-scripts/main.sh
@@ -1,3 +1,23 @@
+#!/bin/bash
+USERNAME=appuser
+for i in "$@"
+do
+case $i in
+    -U=*|--username=*)
+    USERNAME="${i#*=}"
+    shift # past argument=value
+    ;;
+    *)
+          # unknown option
+    ;;
+esac
+done
+getent passwd "${USERNAME}" > /dev/null
+if [ $? -eq 0 ]; then
+    echo "Using username  ${USERNAME}"
+else
+    echo "Specified user ${USERNAME} does not exist"
+fi
 curl https://raw.githubusercontent.com/natalka1122/infra/master/config-scripts/install_mongodb.sh | bash
-curl https://raw.githubusercontent.com/natalka1122/infra/master/config-scripts/install_ruby_deploy.sh | sudo -u appuser -H bash
+curl https://raw.githubusercontent.com/natalka1122/infra/master/config-scripts/install_ruby_deploy.sh | sudo -u "${USERNAME}" -H bash
 curl https://raw.githubusercontent.com/natalka1122/infra/master/config-scripts/service_install.sh | bash
