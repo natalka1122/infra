48540682987221bd4ec77512d1eba89a083d83ec
natalka1122 <natalka1122@yandex.ru>
2019-11-01 09:25:07 +0300

    fix userdir in main.sh

--- config-scripts/main.sh
+++ config-scripts/main.sh
@@ -1,4 +1,4 @@
 curl https://raw.githubusercontent.com/natalka1122/infra/master/config-scripts/install_mongodb.sh | sudo bash
-curl https://raw.githubusercontent.com/natalka1122/infra/master/config-scripts/install_ruby.sh | sudo -u appuser -H bash
-curl https://raw.githubusercontent.com/natalka1122/infra/master/config-scripts/deploy.sh | sudo -u appuser -H bash
+curl https://raw.githubusercontent.com/natalka1122/infra/master/config-scripts/install_ruby.sh | sudo -u appuser -H -i bash
+curl https://raw.githubusercontent.com/natalka1122/infra/master/config-scripts/deploy.sh | sudo -u appuser -H -i bash
 curl https://raw.githubusercontent.com/natalka1122/infra/master/config-scripts/install_service.sh | sudo bash
