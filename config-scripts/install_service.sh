b6faeb3af151622c7e8fed177c224e394af49eac
natalka1122 <natalka1122@yandex.ru>
2019-11-01 09:25:45 +0300

    remove sudo in install_service.sh

--- config-scripts/install_service.sh
+++ config-scripts/install_service.sh
@@ -1,4 +1,4 @@
-sudo wget -O /etc/systemd/system/puma.service https://raw.githubusercontent.com/natalka1122/infra/master/config-scripts/puma.service
-sudo systemctl daemon-reload
-sudo systemctl enable puma.service
-sudo systemctl start puma.service
+wget -O /etc/systemd/system/puma.service https://raw.githubusercontent.com/natalka1122/infra/master/config-scripts/puma.service
+systemctl daemon-reload
+systemctl enable puma.service
+systemctl start puma.service
