69cc4e2c74e5dc99b69620a034286b23afffc8c1
natalka1122 <natalka1122@yandex.ru>
2019-11-12 14:20:24 +0300

    deleted old files

--- terraform/files/deploy.sh
+++
@@ -1,4 +0,0 @@
-cd
-git clone https://github.com/natalka1122/reddit
-cd reddit && bundle install
-wget -O /tmp/puma.service https://raw.githubusercontent.com/natalka1122/infra/master/config-scripts/puma.service
--- terraform/files/install_service.sh
+++
@@ -1,4 +0,0 @@
-sudo mv /tmp/puma.service /etc/systemd/system/puma.service
-sudo systemctl daemon-reload
-sudo systemctl enable puma.service
-sudo systemctl start puma.service
